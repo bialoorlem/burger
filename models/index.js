3271d808409e490ad818b6be4674a6e1822c8ae7
Elliot Trejo <elliot.trejo.tcu@gmail.com>
2020-01-07 11:38:36 -0600

    Fixed the pathing issue with the config in index.js. Now working on local machine. Will need to test on Heroku.

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || "development";
-const config = require("../config.json")[env];
+const config = require(__dirname + "/../config.json")[env];
 const db = {};
 
 let sequelize;
@@ -29,7 +29,7 @@ fs
     return (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js");
   })
   .forEach(function(file) {
-    let model = sequelize["import"](path.join(file));
+    let model = sequelize["import"](path.join(__dirname, file));
     db[model.name] = model;
   });
 
